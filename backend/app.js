95cf56ae9ffcfa7dd0f3f16e83a0db87dd8cc6db
yazhiniii20 <160877024+yazhiniii20@users.noreply.github.com>
2026-06-27 21:26:12 +0530

    Setup Express server

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("Feature flag management app is running");
+});
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const app = require('./app');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
